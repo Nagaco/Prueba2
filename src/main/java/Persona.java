1374056f16c905386094c9ade5967b96540bc6fa
Nagaco <nachogarcor@gmail.com>
2022-05-19 15:35:30 +0200

    Initial commit

---
+++ src/main/java/Persona.java
@@ -0,0 +1,16 @@
+public class Persona
+{
+    private String name;
+
+    public Persona(String name)
+    {
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    
+}
