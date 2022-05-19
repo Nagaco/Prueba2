c0e57f76b2e3a9dbd5b741dd3456472697f11129
Nagaco <nachogarcor@gmail.com>
2022-05-19 15:55:48 +0200

    Prueba2

---
+++ src/test/java/PersonaTest.java
@@ -0,0 +1,15 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+class PersonaTest
+{
+    @Test
+    void getName()
+    {
+        Persona p = new Persona("James");
+        assertEquals("James", p.getName());
+        assertEquals("John", p.getName());
+    }
+}
